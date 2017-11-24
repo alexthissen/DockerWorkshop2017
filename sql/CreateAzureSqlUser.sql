d6e98c91d9707346c602d4919d32ead52c22f8cf
Alex Thissen <athissen@xpirit.com>
2017-11-24 20:51:55 +0100

    Added SQL files and composition for Azure without Azure SQL database (for now)

---
+++ sql/CreateAzureSqlUser.sql
@@ -0,0 +1,14 @@
+﻿-- yourdatabase.database.windows.net,1433
+
+CREATE LOGIN sdp2017 WITH PASSWORD='abc123!@'
+
+USE LeaderboardNETCore
+GO
+
+CREATE USER sdp2017
+	FOR LOGIN sdp2017
+	WITH DEFAULT_SCHEMA = dbo
+GO
+-- Add user to the database owner role
+EXEC sp_addrolemember N'db_owner', N'sdp2017'
+GO
